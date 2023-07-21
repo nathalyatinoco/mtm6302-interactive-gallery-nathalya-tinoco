1060788acb38a0892b249e07fd9582876a934c9f
Nathalya Tinoco <tino0004@algonquinlive.com>
2023-07-21 18:24:52 -0400

    updates html

--- js/script.js
+++ js/script.js
@@ -2,75 +2,75 @@
 const rules = [
     {
         name: 'Card1',
-        desc: 'Rule 1',
+        desc: 'Rule 1: Define love before you think it, feel it or say it.',
         thumb: "images/img-1-thumb.jpg",
-        img: "images/img-1.JPG"
+        img: "images/img-1.jpg"
     },
     {
         name: 'Card2',
-        desc: 'Rule 2',
+        desc: 'Rule 2: Let yourself be alone.',
         thumb: "images/img-2-thumb.jpg",
-        img: "images/img-2.JPG"
+        img: "images/img-2.jpg"
     },
     {
         name: 'Card3',
-        desc: 'Rule 3',
+        desc: 'Rule 3: Your purpose comes first',
         thumb: "images/img-3-thumb.jpg",
-        img: "images/img-3.JPG"
+        img: "images/img-3.jpg"
     },
     {
         name: 'Card4',
-        desc: 'Rule 4',
+        desc: 'Rule 4: Win or lose together.',
         thumb: "images/img-4-thumb.jpg",
-        img: "images/img-4.JPG"
+        img: "images/img-4.jpg"
     },
     {
         name: 'Card5',
-        desc: 'Rule 5',
+        desc: 'Rule 5: Do unto others as you would have them do unto you',
         thumb: "images/img-5-thumb.jpg",
-        img: "images/img-5.JPG"
+        img: "images/img-5.jpg"
     },
     {
         name: 'Card6',
-        desc: 'Rule 6',
+        desc: 'Rule 6: Never invalidate or erase the personal reality of someone you love',
         thumb: 'images/img-6-thumb.jpg',
-        img: "images/img-6.JPG"
+        img: "images/img-6.Jjpg"
     },
     {
         name: 'Card7',
-        desc: 'Rule 7',
+        desc: 'Rule 7: Bids for connection are always honored.',
         thumb: 'images/img-7-thumb.jpg',
-        img: "images/img-7.JPG"
+        img: "images/img-7.jpg"
     },
     {
         name: 'Card8',
-        desc: 'Rule 18',
+        desc: 'Rule 8: The partners in a great relationship are a team',
         thumb: 'images/img-8-thumb.jpg',
-        img: "images/img-8.JPG"
+        img: "images/img-8.jpg"
     },
     {
         name: 'Card9',
-        desc: 'Rule 9',
+        desc: 'Rule 9: People who love each other want to be the best they can be for the other.',
         thumb: 'images/img-9-thumb.jpg',
-        img: "images/img-9.JPG"
+        img: "images/img-9.jpg"
     },
     {
         name: 'Card10',
-        desc: 'Rule 10',
+        desc: 'Rule 10: Ownership or possessiveness is unacceptable',
         thumb: 'images/img-10-thumb.jpg',
-        img: "images/img-10.JPG"
+        img: "images/img-10.jpg"
     },
     {
         name: 'Card11',
-        desc: 'Rule 11',
+        desc: 'Rule 11: Never blame the other partner for what you cannot be, have, or achieve in your own life.',
         thumb: 'images/img-11-thumb.jpg',
-        img: "images/img-11.JPG"
+        img: "images/img-11.jpg"
     },
     {
         name: 'Card12',
-        desc: 'Rule 12',
+        desc: 'Rule 12: Love and love again',
         thumb: 'images/img-12-thumb.jpg',
-        img: "images/img-12.JPG"
+        img: "images/img-12.jpg"
     }
 ];
 
