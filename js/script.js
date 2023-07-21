ac7c73a0795a88d6d20e14b514b405cdf6bd5613
Nathalya Tinoco <tino0004@algonquinlive.com>
2023-07-21 09:06:42 -0400

    renames

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-const galleryContainer = document.querySelector(".gallery-filter")
-for(const galleryContainer of galleryContainers) {
-    galleryContainer.addEventListener("click", openModal)
-}
-
-function openModal () {
-    console.log(this.getAttribute("src"))
-    const fullImage = this.dataset.fullimg
-    const modalBody = document.querySelector(".modal-body")
-  
-    modalBody.innerHTML = `<img src="${fullImage}" alt="placeholder">`
-  }
 
